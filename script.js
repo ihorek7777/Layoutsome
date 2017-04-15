b52a841a008cfe03bdb28f9470e9a33c85383bc2
ihorek7777 <ihor.verkhohliad@nure.ua>
2017-04-15 16:41:56 +0300

    update

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 //document.body.style.zoom = 0.9;
 
-
-document.querySelector('nav ul').onclick = function() {
+var button = document.querySelector('button');
+var menu = document.querySelector('header #main-menu #menu');
+button.onclick = () => {
+	var coords = button.getBoundingClientRect();
 	
-}
+	if (menu.style.display == 'block') {
+		menu.style.display = 'none';
+	} else {
+		menu.style.left = coords.left - 80 + 'px';
+		menu.style.top = coords.top + 'px';
+
+		menu.style.display = 'block';
+		menu.style.opacity = 0.5;	
+	}
+};
+
+window.onresize = () => {
+	menu.style = null;
+};
